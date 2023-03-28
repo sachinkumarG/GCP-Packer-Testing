00840843e399578441ef496a515039281612f40e
sachinkumarG <sachin.gouri@gmail.com>
2023-03-28 20:04:40 +0530

    Add files via upload

---
+++ scripts/agent_install.ps1
@@ -0,0 +1,16 @@
+﻿<#$downloadfolder = 'C:\packer\'
+$checkdir = Test-Path -Path $downloadfolder
+if ($checkdir -eq $false){
+    Write-Verbose "Creating '$downloadfolder' folder"
+    New-Item -Path $downloadfolder -ItemType Directory | Out-Null
+}
+else {
+    Write-Verbose "Folder '$downloadfolder' already exists."
+}
+
+Move-Item C:\Users\sgourimath\Downloads\packer_1.8.6_windows_386\agent\ot_zabbix_agent.exe -Destination $downloadfolder
+#>
+cd c:\
+.\ot_zabbix_agent.exe /S /HOSTNAME=test.cloud.opentext.com /IP=10.59.1.100 /DC=lit
+Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
+GCESysprep
